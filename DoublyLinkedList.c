3cc94463a92246dfbca0db02123153266cc7268c
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2022-05-24 09:59:08 +0530

    Menu Driven code for DLL in C

--- DoublyLinkedList.c
+++ DoublyLinkedList.c
@@ -25,7 +25,7 @@ int NumberOfNodes() {
 		return count; 
 	}
 	else {
-		while(temp->next) {
+		while(temp) {
 			count++;
 			temp=temp->next; 
 		}
@@ -129,10 +129,74 @@ void InsertAtPosition() {
 				temp=temp->next; 
 			}
 			
-			newNode->prev =  temp; 
-			newNode->next = temp->next; 
-			temp->next->prev = newNode; 
-			temp->next = newNode; 
+			temp->next->prev = newNode;
+			newNode->next = temp->next;
+			newNode->prev = temp;
+			temp->next = newNode;
+
+		}
+	}
+}
+
+void DeleteAtBeginning() {
+	struct node *temp;
+	if(temp == NULL) {
+		printf("\tDeletion not possible, since list is empty\n");
+	}
+	else {
+		temp = head; 
+		head = head->next; 
+		if(head!=NULL) {
+			head->prev = NULL; 
+		}
+		free(temp); 
+	}
+}
+
+void DeleteAtEnd() {
+	struct node *temp = head;
+	if(temp == NULL) {
+		printf("\tDeletion not possible, since the list is empty\n");
+	}
+	else {
+		while(temp->next!=NULL) {
+			temp=temp->next;
+		}
+		
+		temp->prev->next = NULL;
+		free(temp); 
+	}
+}
+
+void DeleteAtPosition() {
+	struct node *temp = head;
+	int i=1, value, position; 
+	if(temp == NULL) {
+		printf("Deletion not possible, since list is empty\n");
+	}
+	else {
+		printf("Enter position: ");
+		scanf("%d", &position);
+		
+		if(position > NumberOfNodes()) {
+			printf("\tInvalid position\n"); 
+		}
+		else if(position ==1 ) {
+			DeleteAtBeginning();
+		}
+		else if(position == NumberOfNodes()) {
+			DeleteAtEnd();
+		}
+		else {
+			while(i<position-1) {
+				i++;
+				temp=temp->next; 
+			}
+			struct node *pos = temp->next; 
+			pos->next->prev = temp;
+			temp->next = pos->next;
+		
+			free(pos);  
 		}
 	}
 }
@@ -140,7 +204,7 @@ void InsertAtPosition() {
 int main() {
 	int choice;
 	printf("--------------->> Menu <<--------------\n"); 
-	printf("\t1.Traverse the list\n\t2.Insert at the beginning\n\t3.Insert at the end\n\t4.Insert at position\n\t5.Eixt\n");
+	printf("\t1.Traverse the list\n\t2.Insert at the beginning\n\t3.Insert at the end\n\t4.Insert at position\n\t5.Number of nodes\n\t6.Delete at Beginning\n\t7.Delete at the end\n\t8.Delete at a position\n\t9.Exit Menu\n");
 	 
 	while(1) {
 		printf("Enter your choice: ");
@@ -159,8 +223,20 @@ int main() {
 			case 4: printf("\tYou choose to Insert at a position\n");
 				InsertAtPosition();
 				break;
-			case 5: printf("\tYou are out Menu\n");
+			case 5: printf("\t\tNuber of nodes = %d\n", NumberOfNodes()); 
+				break;
+			case 6: printf("\tYou choose to Delete at the Beginning\n");
+				DeleteAtBeginning();
+				break;
+			case 7: printf("\tYou choose to Delete at the end\n");
+				DeleteAtEnd();
+				break;
+			case 8: printf("\tYou choose to Delete at a position\n"); 
+				DeleteAtPosition();
+				break;
+			case 9: printf("\tYou are out of Menu\n"); 
 				exit(0);
+				break; 
 			default: printf("\tYou choose an INVALID choice, please choose a VALID choice\n");
 
 		}
