2c35cc7a0ca24b582f5a89c9cbf33c92126960dd
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2022-05-24 15:12:36 +0530

    shell sort updated

--- Sortings/shellSort.c
+++ Sortings/shellSort.c
@@ -1,64 +1,42 @@
-// implementation of 'shell sort' in c 
-
 #include<stdio.h>
-
-void printArray(int [], int);
-void shellSort(int[], int); 
-
-void printArray(int array[], int size) {
-	for(int i=0; i<size; i++) {
-		printf("%d ", array[i]); 
-	}
-	printf("\n"); 
-}
-
-void shellSort(int array[], int size) {
-	for(int i=size/2; i>0; i/=2) {
-		for(int j=i; j<size; i+=1) {
-			int temp = array[j];
-			int k;
-		
-			for(k=j; k>=i && array[k-i] > temp; k-=i) {
-				array[k] = array[k-i];
-			}	
-			array[k] = temp; 
-		}
-	}
-}
-
-
-int main() {
-	int size;
-	printf("Enter size: ");
-	scanf("%d", &size);
-	
-	int array[size];
-	printf("Enter array elements: ");
-	for(int i=0; i<size; i++) {
-		scanf("%d", &array[i]);
-	}
-	
-	printf("Array before sortion: ");
-	printArray(array, size);
-	
-	printf("Array after sortion: ");
-	shellSort(array, size);
-	printArray(array, size); 
-
+void shellsort(int [],int);
+int main()
+{
+	int n;
+	printf("enter n value\n");
+	scanf("%d",&n);
+	int a[n],i;
+	printf("enter elements\n");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	printf("elements before sorting\n");
+	for(i=0;i<n;i++)
+	printf("%d ",a[i]);
+	printf("\n");
+	shellsort(a,n);
 	return 0;
 }
-
-/*
---------------------------------------
-Analysis of shell sort algo: 
-	Analysis of time complexity: 
-		Best case: O(n*logn)
-		Average case: O(n*logn)
-		Worst case: O(n^2); 
-	
-	Analysis of space complexity: O(1) 
-	
-	Stability: No
-
----------------------------------------
-*/
+void shellsort(int a[],int n)
+{
+	int i,j,gap,temp;
+	for(gap=n/2;gap>=1;gap=gap/2)
+	{
+		for(j=gap;j<n;j++)
+		{
+			for(i=j-gap;i>=0;i=i-gap)
+			{
+				if(a[i]>a[i+gap])
+				{
+					temp=a[i];
+					a[i]=a[i+gap];
+					a[i+gap]=temp;
+				}
+				else
+				break;
+			}
+		}
+	}
+	printf("elements after sorting:\n");
+	for(i=0;i<n;i++)
+	printf("%d ",a[i]);
+}
